eecd86687a93cafeb78fdf8071ededd04a0e5820
Cody Bouscaren <codybousc@gmail.com>
2015-08-28 13:10:33 -0700

    Works for full size images. Slight issue with previous button on cycle

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
 var backgroundImages = [
-  "url(css/images/1.jpg)"
+  "url(css/images/1.jpg)",
+  "url(css/images/2.jpg)",
+  "url(css/images/3.jpg)",
+  "url(css/images/4.jpg)",
+  "url(css/images/5.jpg)",
+  "url(css/images/6.jpg)",
+  "url(css/images/7.jpg)"
 ];
 
 var sections = [
   {
       id: 'one',
       im: backgroundImages[0]
+  },
+  {
+      id: 'two',
+      im: backgroundImages[1]
+  },
+  {
+      id: 'three',
+      im: backgroundImages[2]
+  },
+  {
+      id: 'four',
+      im: backgroundImages[3]
+  },
+  {
+      id: 'five',
+      im: backgroundImages[4]
+  },
+  {
+      id: 'six',
+      im: backgroundImages[5]
+  },
+  {
+      id: 'seven',
+      im: backgroundImages[6]
   }
 ];
 
-var slideshow;
-
+var slideshow,
+    currSlide = 0;
     function init () {
       var wrapperEl = document.getElementById('wrapper');
       slideshow = document.createElement('div');
@@ -23,7 +53,6 @@ var slideshow;
         newDiv.id = sections[i].id;
         newDiv.className = "slide";
          newDiv.style.backgroundImage = sections[i].im;
-        newDiv.style.backgroundImage.height = 300 + 'px';
         slideshow.appendChild(newDiv);
         sections[i].domElem = newDiv;
     }
@@ -32,42 +61,35 @@ var slideshow;
     var previousButton = document.getElementById("previousButton");
     previousButton.addEventListener('click', prevSlide);
 
+    window.onresize = resize.bind(this);
+    resize.call(this);
+
 }
 
-var j = 2; //moved ths var outside of the function and it now works!
-//My nextSlide Function
+
+
+function gotoSlide(num) {
+  currSlide = num;
+  TweenMax.to(slideshow, 0.65, {left: (-num * window.innerWidth) + 'px', ease: Expo.easeInOUt})
+}
+
 function nextSlide() {
-  //var i = 2;
-  if (j < 8) {
-  var firstPhoto = document.getElementById("one");
-    firstPhoto.setAttribute("style", "background-image: url(css/images/"+ j +".jpg)");
-    j++;
-  }
-  else if (j == 8) {
-    j = 1;
-    //firstPhoto.setAttribute("style", "background-image: url(css/images/"+ j +".jpg)");
-  }
+  gotoSlide((currSlide + 1)%sections.length);
 }
-
-//for the previousSlide function, how do i assign the 'current photo' to a variable that i can then decrement?
-//Idea: since i'm only changing out the photo number in the first image with an id of "one" i could potentially
-//grab the current photo number from that id?
-
 
 function prevSlide() {
-  var prevPhoto = document.getElementById("one");
-  var firstPhotoAttr = prevPhoto.attributes[2]
-  var nextAttrVal = firstPhotoAttr.value
-  var nextAttrValArray = nextAttrVal.split("");
-  var currentPhoto = nextAttrValArray[33]; //seems to be ahead by one
-  var previousPhoto = currentPhoto - 1;
-  if (currentPhoto > 1) {
-    prevPhoto.setAttribute("style", "background-image: url(css/images/"+ previousPhoto +".jpg)");
-    console.log(currentPhoto)
-  }
-  else if (currentPhoto == 1) { //starts the animation from zero once last pic is reached. slight bug though, have to click twice.
-    prevPhoto.setAttribute("style", "background-image: url(css/images/"+ 7 +".jpg)");
-  }
+  gotoSlide((currSlide - 1)%sections.length);
+}
+
+function resize(){
+    var w = window.innerWidth,
+        h = window.innerHeight;
+
+    for(var i = 0; i < sections.length; i++) {
+      sections[i].domElem.style.width = w + 'px';
+      sections[i].domElem.style.height = h + 'px';
+      sections[i].domElem.style.left = (w * i) + 'px';
+    }
 }
 
 // call init on document ready
